b3bdf555aba1185bb12793cd0d920ea874adcd9a
Ariel Cavalcante <ariel.cavalcante@edu.unifor.br>
2021-03-09 11:40:28 -0300

    Adiciona form de contato -funcionando

---
+++ index.js
@@ -0,0 +1,4 @@
+const nome = document.querySelector('#nome');
+const email = document.querySelector('#email');
+const mensagem = document.querySelector('#mensagem');
+const enviar = document.querySelector('form button');
