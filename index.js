9e4b62cb89e37de779ceb8446de6ac9f6ba0712d
Ariel Cavalcante <ariel.cavalcante@edu.unifor.br>
2021-03-11 23:42:08 -0300

    add main styles, functioning contact form and blog section

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const nome = document.querySelector('#nome');
-const email = document.querySelector('#email');
-const mensagem = document.querySelector('#mensagem');
-const enviar = document.querySelector('form button');
+// const nome = document.querySelector('#element_0');
+// const email = document.querySelector('#element_1');
+// const mensagem = document.querySelector('#element_2');
+// const enviar = document.querySelector('form button.primary-button');
+// const form = document.querySelector('form');
+
+const openMenu = document.querySelector('.menu-button');
+const closeMenu = document.querySelector('.close-button');
+const menu = document.querySelector('header nav');
+
+function activateMenu() {
+	if (menu.style.display === 'none') menu.style.display = 'flex';
+	else menu.style.display = 'none';
+}
+
+openMenu.addEventListener('click', activateMenu);
+closeMenu.addEventListener('click', activateMenu);
